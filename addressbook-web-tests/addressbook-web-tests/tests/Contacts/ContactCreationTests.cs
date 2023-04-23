014b4c8f4117472e6a21d136ada93328b0c4488d
Mikael <mikaelsparrow@gmail.com>
2023-04-23 21:57:41 +0700

    Добавлена генерация тестовых данных для тестов создания групп и контактов.

--- addressbook-web-tests/addressbook-web-tests/tests/Contacts/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/Contacts/ContactCreationTests.cs
@@ -10,11 +10,21 @@ namespace WebAddressbookTests
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        [Test]
-        public void ContactCreationTest()
+        public static IEnumerable<ContactData> RandomContactDataProvider()
         {
-            ContactData contact = new ContactData("Name");
-            contact.LastName = "Surname";
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(30))
+                {
+                    LastName = GenerateRandomString(30)
+                });
+            }
+            return contacts;
+        }
+        [Test, TestCaseSource("RandomContactDataProvider")]
+        public void ContactCreationTest(ContactData contact)
+        {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
 
             app.Contacts.Create(contact);
--- addressbook-web-tests/addressbook-web-tests/tests/Groups/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/Groups/GroupCreationTests.cs
@@ -10,46 +10,23 @@ namespace WebAddressbookTests
     [TestFixture]
     public class GroupCreationTests : AuthTestBase
     {
-        [Test]
-        public void GroupCreationTest()
+        public static IEnumerable<GroupData> RandomGroupDataProvider()
         {
-            GroupData group = new GroupData("new");
-            group.Header = "header";
-            group.Footer = "footer";
-            List<GroupData> oldGroups = app.Groups.GetGroupList();
-
-            app.Groups.Create(group);
-            List<GroupData> newGroups = app.Groups.GetGroupList();
-            oldGroups.Add(group);
-            oldGroups.Sort();
-            newGroups.Sort();
-            Assert.AreEqual(oldGroups, newGroups);
-        }
-
-        [Test]
-        public void EmptyGroupCreationTest()
-        {
-            GroupData group = new GroupData("")
+            List<GroupData> groups = new List<GroupData>();
+            for (int i = 0; i < 5; i++)
             {
-                Header = "",
-                Footer = ""
-            };
-            List<GroupData> oldGroups = app.Groups.GetGroupList();
-
-            app.Groups.Create(group);
-            List<GroupData> newGroups = app.Groups.GetGroupList();
-            oldGroups.Add(group);
-            oldGroups.Sort();
-            newGroups.Sort();
-            Assert.AreEqual(oldGroups, newGroups);
+                groups.Add(new GroupData(GenerateRandomString(30))
+                {
+                    Header = GenerateRandomString(100),
+                    Footer = GenerateRandomString(100)
+                });
+            }
+            return groups;
         }
 
-        [Test]
-        public void BadNameGroupCreationTest()
+        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public void GroupCreationTest(GroupData group)
         {
-            GroupData group = new GroupData("k'pp");
-            group.Header = "";
-            group.Footer = "";
             List<GroupData> oldGroups = app.Groups.GetGroupList();
 
             app.Groups.Create(group);
